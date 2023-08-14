b84c894c13a18f8056399e7f9b447d83fd769122
Emily <emily@cu-genvpn-comp-10.180.45.85.int.colorado.edu>
2023-08-14 13:20:15 -0600

    added hmo clustering changes

--- 3-cluster-analysis.R
+++ 3-cluster-analysis.R
@@ -23,18 +23,18 @@ pacman::p_load(knitr, tidyverse, magrittr, lme4, lmerTest, GGally, corrplot,
                readxl, factoextra, ggbiplot, dendextend)
 
 #set the input folder
-data_in <- "/Volumes/IPHY/ADORLab/__Users/emye7956/MM/HMO-miRNA/1-data-cleaning/rda"
+#data_in <- "/Volumes/IPHY/ADORLab/__Users/emye7956/MM/HMO-miRNA/1-data-cleaning/rda"
 
 #set the output folder
-figs_out <- "/Volumes/IPHY/ADORLab/__Users/emye7956/MM/HMO-miRNA/1-data-cleaning/figs"
+#figs_out <- "/Volumes/IPHY/ADORLab/__Users/emye7956/MM/HMO-miRNA/1-data-cleaning/figs"
 
 #read in the clean miRNA_cpm data
-miRNA_cpm <- read.csv(file = paste0(data_in, "miRNA_counts.csv"))
-miRNA_cpm <- read.csv("input/miRNA_counts.csv")
+#miRNA_cpm <- read.csv(file = paste0(data_in, "miRNA_counts.csv"))
+miRNA_cpm <- read.csv("input/miRNA_counts_EY.csv")
 
 #read in the clean meta data
-meta <- read.csv(file = paste0(data_in, "meta_clean_bl.csv"))
-meta <- read.csv("input/meta_clean_bl.csv")
+#meta <- read.csv(file = paste0(data_in, "meta_clean_bl.csv"))
+meta <- read.csv("input/meta_clean_EY.csv")
 
 # PREPARE THE DATA SET ---------------------------------------------------------
 # drop unnecessary columns
@@ -50,7 +50,7 @@ miRNA_cpm_trim <- dplyr::select(miRNA_cpm, -c("X", "Sample_ID", "prop_rRNA",
 # make a second miRNA cpm object with only secretors
 table(meta$Secretor)
 #No Yes 
-#59 466
+#24 183 
 secretors <- meta$dyad_id[which(meta$Secretor == "Yes")]
 
 # only get miRNA of secretors to test whether clustering is better
@@ -135,6 +135,7 @@ cluster_3 <- hclust[hclust == 3]
 meta$cluster <- NA
 meta$cluster[row.names(meta) %in% names(cluster_1)] <- 1
 meta$cluster[row.names(meta) %in% names(cluster_2)] <- 2
+meta$cluster[row.names(meta) %in% names(cluster_3)] <- 3
 
 summary(meta$cluster)
 
@@ -145,14 +146,21 @@ hc <- meta[which(!is.na(meta$cluster)),]
 # make a list of vars to include
 vars <- c("mom_age_at_birth", "SES", "baby_gender_cat", "mode_of_delivery_cat", 
           "age_in_days", "pred_bf", "breastfeedings_continuous", "mom_BMI",
-          "Secretor", "Diversity", "Sia", "Fuc", "SUM..nmol.mL.", 
-          "X2.FL..nmol.mL.", "X3FL..nmol.mL.", "X3.SL..nmol.mL.", 
-          "X6.SL..nmol.mL.", "DFLac..nmol.mL.", "DFLNH..nmol.mL.", 
-          "DFLNT..nmol.mL.", "DSLNH..nmol.mL.", "DSLNT..nmol.mL.",
-          "FDSLNH..nmol.mL.", "FLNH..nmol.mL.", "LNnT..nmol.mL.", 
-          "LNT..nmol.mL.", "LNFP.I..nmol.mL.", "LNFP.II..nmol.mL.", 
-          "LNFP.III..nmol.mL.", "LNH..nmol.mL.", "LSTb..nmol.mL.", 
-          "LSTc..nmol.mL.", "breast_milk_time_hrs")
+          "Secretor", "Diversity", "Sia", "Fuc", #"Secretor", 
+          "x2FL_nmol_ml","x3FL_nmol_ml",
+          "LNnT_nmol_ml",
+          "x3SL_nmol_ml","DFLac_nmol_ml",
+          "x6SL_nmol_ml","LNT_nmol_ml","LNFP_I_nmol_ml",
+          "LNFP_II_nmol_ml","LNFP_III_nmol_ml",
+          "LSTb_nmol_ml","LSTc_nmol_ml","DFLNT_nmol_ml",
+          "LNH_nmol_ml",
+          "DSLNT_nmol_ml","FLNH_nmol_ml","DFLNH_nmol_ml",
+          "FDSLNH_nmol_ml",
+          "DSLNH_nmol_ml","SUM_nmol_ml",
+          "Sia_nmol_ml","Fuc_nmol_ml","x2FL_ug_ml",
+          "x3FL_ug_ml","LNnT_ug_ml",
+          "x3SL_ug_ml", "DFLac_ug_ml", "x6SL_ug_ml", 
+          "LNT_ug_ml", "LNFP_I_ug_ml", "breast_milk_time_hrs")
 #CHANGE OLD NAMES
 
 vars <- unlist(vars)
@@ -160,10 +168,11 @@ vars <- unlist(vars)
 # make separate data sets for clusters 1 and 3
 cluster_1 <- hc[which(hc$cluster == 1),]
 cluster_2 <- hc[which(hc$cluster == 2),]
+cluster_3 <- hc[which(hc$cluster == 3),]
 
 # make an output table
-cluster_out <- data.frame(matrix(ncol = 4, nrow = 0))
-colnames(cluster_out) <- c("Var", "Cluster_1", "Cluster_2", "p")
+cluster_out <- data.frame(matrix(ncol = 5, nrow = 0))
+colnames(cluster_out) <- c("Var", "Cluster_1", "Cluster_2", "Cluster_3", "p")
 
 # loop over vars, run t-tests or chi-squared tests for each
 for(thisVar in vars){
@@ -188,6 +197,9 @@ for(thisVar in vars){
     cluster_out[i, "Cluster_2"] <- paste0(round(temp$estimate[2], digits = 2), 
                                           " ± ",
                                           round(sd(cluster_2[,thisVar]), digits = 2))
+    cluster_out[i, "Cluster_3"] <- paste0(round(temp$estimate[2], digits = 2), 
+                                          " ± ",
+                                          round(sd(cluster_3[,thisVar]), digits = 2))
     cluster_out[i, "p"] <- temp$p.value
   }
 }
@@ -198,16 +210,19 @@ cluster_out$pfdr <- p.adjust(cluster_out$p) # no sig diff
 # get the n for the categorical variables in each cluster
 table(cluster_1$baby_gender_cat)
 table(cluster_2$baby_gender_cat)
+table(cluster_3$baby_gender_cat)
 
 table(cluster_1$mode_of_delivery_cat)
+table(cluster_2$mode_of_delivery_cat)
 table(cluster_2$mode_of_delivery_cat)
 
 table(cluster_1$pred_bf)
 table(cluster_2$pred_bf)
+table(cluster_3$pred_bf)
 
 table(cluster_1$Secretor)
 table(cluster_2$Secretor)
-
+table(cluster_3$Secretor)
 
 # SENSITIVITY ANALYSIS - SECRETORS ---------------------------------------------
 
@@ -228,14 +243,21 @@ hc <- meta[which(!is.na(meta$cluster)),]
 # remove secretor from vars
 vars <- c("mom_age_at_birth", "SES", "baby_gender_cat", "mode_of_delivery_cat", 
           "age_in_days", "pred_bf", "breastfeedings_continuous", "mom_BMI",
-          "Diversity", "Sia", "Fuc", "SUM..nmol.mL.", 
-          "X2.FL..nmol.mL.", "X3FL..nmol.mL.", "X3.SL..nmol.mL.", 
-          "X6.SL..nmol.mL.", "DFLac..nmol.mL.", "DFLNH..nmol.mL.", 
-          "DFLNT..nmol.mL.", "DSLNH..nmol.mL.", "DSLNT..nmol.mL.",
-          "FDSLNH..nmol.mL.", "FLNH..nmol.mL.", "LNnT..nmol.mL.", 
-          "LNT..nmol.mL.", "LNFP.I..nmol.mL.", "LNFP.II..nmol.mL.", 
-          "LNFP.III..nmol.mL.", "LNH..nmol.mL.", "LSTb..nmol.mL.", 
-          "LSTc..nmol.mL.", "breast_milk_time_hrs")
+          "Diversity", "Sia", "Fuc", #"Secretor", 
+          "x2FL_nmol_ml","x3FL_nmol_ml",
+          "LNnT_nmol_ml",
+          "x3SL_nmol_ml","DFLac_nmol_ml",
+          "x6SL_nmol_ml","LNT_nmol_ml","LNFP_I_nmol_ml",
+          "LNFP_II_nmol_ml","LNFP_III_nmol_ml",
+          "LSTb_nmol_ml","LSTc_nmol_ml","DFLNT_nmol_ml",
+          "LNH_nmol_ml",
+          "DSLNT_nmol_ml","FLNH_nmol_ml","DFLNH_nmol_ml",
+          "FDSLNH_nmol_ml",
+          "DSLNH_nmol_ml","SUM_nmol_ml",
+          "Sia_nmol_ml","Fuc_nmol_ml","x2FL_ug_ml",
+          "x3FL_ug_ml","LNnT_ug_ml",
+          "x3SL_ug_ml", "DFLac_ug_ml", "x6SL_ug_ml", 
+          "LNT_ug_ml", "LNFP_I_ug_ml", "breast_milk_time_hrs")
 vars <- unlist(vars)
 
 # make separate data sets for clusters 1 and 3
@@ -290,4 +312,107 @@ cluster_sec$pfdr <- p.adjust(cluster_sec$p)
 cluster_sec$pfdr2 <- p.adjust(cluster_sec$p2)
 cluster_sec$pfdr3 <- p.adjust(cluster_sec$p3)
 
+# ADDING HMO CLUSTERING ---------------------------------------------------------
+
+# PREPARE THE DATA SET 
+# drop unnecessary columns
+rownames(meta) <- paste0("X", meta$dyad_id)
+
+#drop meta data
+meta_hmo_trim <- dplyr::select(meta, c("mom_age_at_birth", "SES", "baby_gender_cat", "mode_of_delivery_cat", "age_in_days", "pred_bf", "breastfeedings_continuous", "mom_BMI",
+"Secretor", "Diversity", "Sia", "Fuc", "x2FL_nmol_ml","x3FL_nmol_ml","LNnT_nmol_ml",
+"x3SL_nmol_ml","DFLac_nmol_ml","x6SL_nmol_ml","LNT_nmol_ml","LNFP_I_nmol_ml",
+"LNFP_II_nmol_ml","LNFP_III_nmol_ml","LSTb_nmol_ml","LSTc_nmol_ml","DFLNT_nmol_ml",
+"LNH_nmol_ml", "DSLNT_nmol_ml","FLNH_nmol_ml","DFLNH_nmol_ml","FDSLNH_nmol_ml",
+"DSLNH_nmol_ml","SUM_nmol_ml", "Sia_nmol_ml","Fuc_nmol_ml","x2FL_ug_ml",
+ "x3FL_ug_ml","LNnT_ug_ml","x3SL_ug_ml", "DFLac_ug_ml", "x6SL_ug_ml", 
+"LNT_ug_ml", "LNFP_I_ug_ml", "breast_milk_time_hrs"))
+
+#remove NAs from meta_hmo_trim
+meta_hmo_trim <- meta_hmo_trim[complete.cases(meta_hmo_trim), ] #221 - 207 = 14 cases removed
+
+# make a second miRNA cpm object with only secretors
+table(meta_hmo_trim$Secretor)
+#No Yes 
+#24 183 
+secretors_hmo <- meta$dyad_id[which(meta$Secretor == "Yes")]
+
+# only get hmo of secretors to test whether clustering is better
+meta_hmo_sec <- meta[which(meta$dyad_id %in% secretors_hmo),]
+meta_hmo_sec <- dplyr::select(meta_hmo_sec, c("mom_age_at_birth", "SES", "baby_gender_cat", "mode_of_delivery_cat", "age_in_days", "pred_bf", "breastfeedings_continuous", "mom_BMI",
+"Secretor", "Diversity", "Sia", "Fuc", "x2FL_nmol_ml","x3FL_nmol_ml","LNnT_nmol_ml",
+ "x3SL_nmol_ml","DFLac_nmol_ml","x6SL_nmol_ml","LNT_nmol_ml","LNFP_I_nmol_ml",
+ "LNFP_II_nmol_ml","LNFP_III_nmol_ml","LSTb_nmol_ml","LSTc_nmol_ml","DFLNT_nmol_ml",
+ "LNH_nmol_ml", "DSLNT_nmol_ml","FLNH_nmol_ml","DFLNH_nmol_ml","FDSLNH_nmol_ml",
+"DSLNH_nmol_ml","SUM_nmol_ml", "Sia_nmol_ml","Fuc_nmol_ml","x2FL_ug_ml",
+"x3FL_ug_ml","LNnT_ug_ml","x3SL_ug_ml", "DFLac_ug_ml", "x6SL_ug_ml", 
+"LNT_ug_ml", "LNFP_I_ug_ml", "breast_milk_time_hrs"))
+#183 of 221 with hmo observations were secretors
+
+# drop the extra variables
+meta_hmo_trim_ID <- meta_hmo_trim[,13:42] #221
+hmo_sec_trim_ID <- meta_hmo_sec[,13:42] #183
+
+#make numeric
+meta_hmo_trim <- sapply(meta_hmo_trim_ID, as.numeric)
+hmo_sec_trim <- sapply(hmo_sec_trim_ID, as.numeric)
+
+# scale 
+meta_hmo_trim_scaled <- data.frame(scale(meta_hmo_trim))
+hmo_sec_trim_scaled <- data.frame(scale(hmo_sec_trim))
+
+# now calculate the distance matrix
+dist_mat <- dist(meta_hmo_trim_scaled, method = 'euclidean')
+dist_mat_sec <- dist(hmo_sec_trim_scaled, method = "euclidean")
+#Why Euclidian?
+
+# cluster
+hclust_avg <- hclust(dist_mat, method = "complete")
+hclust_sec <- hclust(dist_mat_sec, method = "complete")
+#revise methods complete 
+
+# make a plot
+dend <- as.dendrogram(hclust_avg)
+dend_sec <- as.dendrogram(hclust_sec)
+
+dend %>% plot
+
+dend_sec %>% plot 
+
+#change the color of the branches to correspond with the clusters
+
+#CHANGE TO SEE WHAT THIS LOOKS LIKE CLUSTERING BY HMO
+dend %>% set("branches_k_color", k = 9) %>%
+  set("labels_cex", 0) %>%
+  plot()
+
+plot(hclust_avg, cex = 0.4)
+
+plot(hclust_sec, cex = 0.4)
+
+# use the "Elbow" method to estimate the appropriate number of clusters
+set.seed(78)
+# Remove columns with NA values
+miRNA_cpm_scaled <- miRNA_cpm_scaled[, colSums(is.na(miRNA_cpm_scaled)) == 0]
+fviz_nbclust(miRNA_cpm_scaled, kmeans, method = "wss", k.max = 10)
+fviz_nbclust(miRNA_sec_trim, kmeans, method = "wss", k.max = 10)
+# 3 or 4 clusters,  based on this
+
+# we should use 9 clusters based on the figure, but that results in several
+# groups having only 1 person in them - try 3 and see how that looks
+# with three, there is still only one individual in the third cluster (X166)
+# try 2
+
+hclust <- cutree(hclust_avg, 6)
+names(hclust) <- miRNA_cpm$dyad_id
+hclust_sec2 <- cutree(hclust_sec, 4)
+
+# get some info about the clustering
+table(hclust)
+#1   2   3   4   5   6 
+#81 101  15   2   1   7 
+table(hclust_sec2)
+hclust_sec2
+#1  2  3  4 
+#99 76  1  7
 
